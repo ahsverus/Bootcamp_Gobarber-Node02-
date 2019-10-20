46bbdacfdfff701115feacaafae96887901135ce
ahsverus <arthursand1411@gmail.com>
2019-10-20 14:53:31 -0300

    add connection to database & User model

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  res.json({ message: 'hello world' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Diego Fernandes',
+    email: 'shuahsuh@hotmail.com',
+    password_hash: '123456',
+  });
+
+  return res.json({ user });
 });
 
 export default routes;
